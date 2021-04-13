410004cef8a534e07cd5ac7bb268aa241da1fce3
sophia2798 <sophia2798@gmail.com>
2021-04-13 14:32:14 -0700

    added route to resolve nodes

--- blockchain.py
+++ blockchain.py
@@ -230,7 +230,23 @@ def register_nodes():
 
     return jsonify(response), 201
 
+# RESOLVE NODES
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+    replaced = blockchain.resolve_conflicts()
 
+    if replaced:
+        response= {
+            'message': 'Our chain was replaced/updated',
+            'new_chain': blockchain.chain
+        }
+    else:
+        response = {
+            'message': 'Our chain is authoritative',
+            'chain': blockchain.chain
+        }
+
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
